705fbc0ee4bc08838cb3a2092d51f639cc06b851
Andre Caetano <webandrecaetano@gmail.com>
2016-07-30 22:59:49 -0300

    liverreload over assets and html

--- gulp/watch.js
+++ gulp/watch.js
@@ -4,17 +4,19 @@ var gulp = require('gulp');
 
 module.exports = function(options) {
 
-	// gulp.task('reload',function(done){
-	// 	if(options.electronServer){
-	// 		options.electronServer.reload();
-	// 	}
-	// 	done();
-	// });
+	gulp.task('reload',function(done){
+		if(options.electronServer){
+			options.electronServer.reload();
+		}
+		done();
+	});
 
 	gulp.task('watch', gulp.series('scripts:watch', function watch(done) {
-		// gulp.watch([
-		// 	options.src+'/**/*.js'
-		// ], gulp.series('reload'));
+		// ! NEED to be split between assets and modules
+		// ! it reduce over 80% the reload time
+		gulp.watch(options.electronFiles.concat([
+			options.src+'/index.html'
+		]), gulp.series('electronFiles','reload'));
 
 		done();
 	}));
